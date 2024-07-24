baaa22e9b7813cebd12e90c1ac42afd25a82eb16
jerrelllzw <lowjerrell47@gmail.com>
2024-07-24 12:31:23 +0800

    Add basic validaton and onclickhandler

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { useState } from 'react';
 import { ChakraProvider, Heading, Flex, Button } from '@chakra-ui/react';
 import { Dropzone, FileMosaic } from '@files-ui/react';
+import Title from './components/Title';
 
-function App() {
+const App = () => {
 	const [files, setFiles] = useState([]);
+
 	const updateFiles = (incomingFiles) => {
-		//do something with the files
-		console.log('incoming files', incomingFiles);
 		setFiles(incomingFiles);
-		//even your own upload implementation
 	};
+
 	const removeFile = (id) => {
 		setFiles(files.filter((x) => x.id !== id));
+	};
+
+	const validateFiles = () => {
+		if (files.length < 2) {
+			alert('Please upload both followers_1.json and following.json files.');
+			return false;
+		}
+		if (
+			(files[0].name === 'followers_1.json' &&
+				files[1].name === 'following.json') ||
+			(files[1].name === 'followers_1.json' &&
+				files[0].name === 'following.json')
+		) {
+			return true;
+		} else {
+			alert('Please upload both followers_1.json and following.json files.');
+			return false;
+		}
+	};
+
+	const handleOnClick = () => {
+		if (validateFiles()) {
+			const readers = files.map((file) => {
+				return new Promise((resolve, reject) => {
+					const reader = new FileReader();
+					reader.onload = (event) => {
+						resolve(JSON.parse(event.target.result));
+					};
+					reader.onerror = (error) => reject(error);
+					reader.readAsText(file.file);
+				});
+			});
+
+			Promise.all(readers).then((contents) => {
+				console.log(contents);
+			});
+		}
 	};
 
 	return (
@@ -23,20 +60,24 @@ function App() {
 				height='100vh'
 				bg='gray.100'
 			>
-				<Heading mb={6}>InstaUnfollow</Heading>
-				<Flex flexDirection='row' justifyContent='center' alignItems='center'>
-					<Dropzone onChange={updateFiles} value={files}>
-						{files.map((file) => (
-							<FileMosaic key={file.id} {...file} onDelete={removeFile} info />
-						))}
-					</Dropzone>
-					<Dropzone onChange={updateFiles} value={files}>
-						{files.map((file) => (
-							<FileMosaic key={file.id} {...file} onDelete={removeFile} info />
-						))}
-					</Dropzone>
-				</Flex>
-				<Button colorScheme='blue'>Button</Button>
+				<Title/>
+				<Dropzone
+					onChange={updateFiles}
+					value={files}
+					accept='.json'
+					maxFiles={2}
+					style={{ width: '50vw' }}
+					label={'Drop your followers_1.json and following.json files here'}
+					color='#6200EE'
+					background='radial-gradient(circle at 18.7% 37.8%, rgb(250, 250, 250) 0%, rgb(225, 234, 238) 90%)'
+				>
+					{files.map((file) => (
+						<FileMosaic key={file.id} {...file} onDelete={removeFile} />
+					))}
+				</Dropzone>
+				<Button mt={6} colorScheme='green' onClick={handleOnClick}>
+					Get Results
+				</Button>
 			</Flex>
 		</ChakraProvider>
 	);
---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+
+const Title = () => {
+	return <Heading mb={6}>InstaUnfollow</Heading>;
+};
+
+export default Title;

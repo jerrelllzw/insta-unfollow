c61a38774961dedc21f94ee15c005f8be0c57da9
jerrelllzw <lowjerrell47@gmail.com>
2024-07-26 07:53:39 +0800

    Add google tag manager

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TagManager from 'react-gtm-module'
+ 
+const tagManagerArgs = {
+    gtmId: 'G-4D6HDJZTYF'
+}
+ 
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
